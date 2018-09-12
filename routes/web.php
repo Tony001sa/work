0059797a9a079aef4c881e8c50678045a5777a22
myfbone <kevin.mitnik.zep@gmial.com>
2018-09-12 22:37:23 +0300

    Base index page template remade for laravel

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('static.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
