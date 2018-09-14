310c8a989bc149e2ddd3229e0d66ab62cbd3c18d
myfbone <kevin.mitnik.zep@gmial.com>
2018-09-14 18:29:49 +0300

    tagline and mail added to temp page

--- resources/views/temp/temp.blade.php
+++ resources/views/temp/temp.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Selfy Pay</title>
-	<link rel="stylesheet" href="{{URL::asset('css/temp/temp.css')}}?v=1">
+	<link rel="stylesheet" href="{{URL::asset('css/temp/temp.css')}}?v=1.01">
 </head>
 <body>
     <div class="main-container">
@@ -16,7 +16,14 @@
             <span class="sp-name">SELFY PAY</span>
         </div>
         <div class="item">
-            <span class="sp-coming-soon">COMING SOON</span>
+            <span class="tagline">
+                ТВОЯ ПЕРСОНАЛЬНАЯ КАССА
+            </span>
+        </div>
+        <div class="item">
+            <span class="mail">
+                info@selfy-pay.ru
+            </span>
         </div>
     </div>
 </body>
